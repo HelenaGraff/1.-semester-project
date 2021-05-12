719ed4f5f7f32decd5124180200813c6ea8efdcd
andr92h1 <andr92h1@edu.easj.dk>
2021-05-12 10:22:48 +0200

    learn-quiz component

--- Mental-Shower-2/mental-shower/src/app/learn/learn-modal/learn-modal.page.spec.ts
+++ Mental-Shower-2/mental-shower/src/app/learn/learn-modal/learn-modal.page.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { LearnModalPage } from './learn-modal.page';
+import { LearnQuizComponent } from 'src/app/components/learn-quiz/learn-quiz.component'
 
 describe('LearnModalPage', () => {
   let component: LearnModalPage;
@@ -10,7 +11,7 @@ describe('LearnModalPage', () => {
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ LearnModalPage ],
-      imports: [IonicModule.forRoot()]
+      imports: [IonicModule.forRoot(), LearnQuizComponent]
     }).compileComponents();
 
     fixture = TestBed.createComponent(LearnModalPage);
