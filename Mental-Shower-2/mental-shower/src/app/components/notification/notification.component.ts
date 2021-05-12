bead757d79fddfc1dd99eec559e42f1a265d22e5
HelenaGraff <45385583+HelenaGraff@users.noreply.github.com>
2021-05-12 14:07:58 +0200

    Did some fixes on the noti

--- Mental-Shower-2/mental-shower/src/app/components/notification/notification.component.ts
+++ Mental-Shower-2/mental-shower/src/app/components/notification/notification.component.ts
@@ -10,6 +10,15 @@ export class NotificationComponent implements OnInit {
   @Input("notificationBody") notificationBody:string;
   @Input("quickFix") quickFix:string;
   @Input("minsAgo") minsAgo:string;
+  @Input("showButton") showButton:boolean;
+  @Input("buttonContent") buttonContent:string;
+  @Input("hoursAgo") hoursAgo:string;
+
+  @Input("minsEmpty") minsEmpty:boolean;
+
+
+
+
   constructor() { }
 
   ngOnInit() {}
