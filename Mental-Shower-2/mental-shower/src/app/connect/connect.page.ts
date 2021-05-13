357e14fddfacb7fcc5f316b96a38167c5ee1f8ab
andr92h1 <andr92h1@edu.easj.dk>
2021-05-13 11:45:49 +0200

    toast and card service

--- Mental-Shower-2/mental-shower/src/app/connect/connect.page.ts
+++ Mental-Shower-2/mental-shower/src/app/connect/connect.page.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-connect',
   templateUrl: './connect.page.html',
   styleUrls: ['./connect.page.scss'],
 })
+
+
 export class ConnectPage implements OnInit {
 
-  constructor() { }
+  constructor(public toastController: ToastController) {}
 
   ngOnInit() {
   }
-
+  async presentToast() {
+    const toast = await this.toastController.create({
+      message: 'Connected successfully',
+      duration: 2000,
+      cssClass: 'cardConnected',
+    });
+    toast.present();
+  }
 }
