5d1171f5ca8f03b624e11f467f84d759a055ec37
Andrei Popa <andreip0pa44@gmail.com>
2021-05-14 14:57:58 +0200

    Update app-routing.module.ts

--- Mental-Shower-2/mental-shower/src/app/app-routing.module.ts
+++ Mental-Shower-2/mental-shower/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Learn',
+    redirectTo: '/learn',
     pathMatch: 'full'
   },
   {
